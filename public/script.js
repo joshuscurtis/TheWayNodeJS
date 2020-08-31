ace3410009d09d7c3e63a29558e06bfe229903c8
Josh <joshuscurtis@gmail.com>
2020-08-31 16:58:33 +0100

    new

--- public/script.js
+++ public/script.js
@@ -621,6 +621,8 @@ function createOrderCardContent(responseObj) {
 	
 	//SLAHighlight(id);
 	
+	
+	
 	//loop through each item in a order
 	for(var y = 0; y < orderData.length; y++) {
 		if((orderData[y].name).substring(0, 5) != "Table") {
