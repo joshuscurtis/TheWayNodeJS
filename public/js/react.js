e45ef68e1fa8c08d9cdcdb14a5e65019b80682ed
Josh <joshuscurtis@gmail.com>
2020-09-05 14:05:34 +0100

    react

--- public/js/react.js
+++ public/js/react.js
@@ -11,37 +11,17 @@ const {
   Toolbar,
   MenuIcon,
   IconButton,
-  makeStyles,
   Button,
 } = MaterialUI;
 
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
 
-function ButtonAppBar() {
-  const classes = useStyles();
+export function ButtonAppBar() {
 
   return (
-    <div className={classes.root}>
+    <div>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </div>
