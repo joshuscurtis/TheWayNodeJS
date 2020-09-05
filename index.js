116f1468090b683cdc7db859ea05b258aa416e09
Josh <joshuscurtis@gmail.com>
2020-09-05 23:31:15 +0100

    react

--- index.js
+++ index.js
@@ -199,7 +199,7 @@ setInterval(function() {
             auth1 = JSON.parse(auth1);
 			
 		//send to pg
-		var thisQuery = "INSERT INTO public.devorders (order_id, products, istable, isnew, isclosed, isprocessing, time, tablenum) VALUES ("+auth1.purchases[0].globalPurchaseNumber+", '" +JSON.stringify(auth1.purchases[0].products)+"',"+doesOrderContainTable(auth1.purchases[0].products)+", "+true+", "+false+", "+false+", "+theTime+",  "+getTableNum(auth1.purchases[0].products)+");"
+		var thisQuery = "INSERT INTO public.devorders (order_id, products, istable, isnew, isclosed, isprocessing, time, tablenum) VALUES ("+auth1.purchases[0].globalPurchaseNumber+", '" +JSON.stringify(auth1.purchases[0].products)+"',"+doesOrderContainTable(auth1.purchases[0].products)+", "+true+", "+false+", "+false+", "+theTime+",'"+getTableNum(auth1.purchases[0].products)+"');"
 		console.log(thisQuery)
 		pool.query(thisQuery, (err, res) => {
 			console.log(err);
