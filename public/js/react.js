22cef190e73f12d9d5ff81f5c9a5e4f1e549f402
Josh <joshuscurtis@gmail.com>
2020-09-05 17:09:38 +0100

    react

--- public/js/react.js
+++ public/js/react.js
@@ -78,6 +78,7 @@ function OrderItem(props) {
 
 function OrderItems(props) {
 	var order = props.order;
+	console.log(order);
 	var rows = [];
 	for (var i = 0; i < 1; i++) {
     	rows.push(<OrderItem itemName={order.product[i].itemName} qty={order.product[i].qty} />);
