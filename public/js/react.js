843879013170941c1783bf3f796ae7bef3868680
Josh <joshuscurtis@gmail.com>
2020-09-05 12:26:44 +0100

    react.js

--- public/js/react.js
+++ public/js/react.js
@@ -1,13 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
 
-function App() {
-  return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-  );
-}
-
-ReactDOM.render(<App />, document.querySelector('#app'));
+ReactDOM.render(<h1>Hello, world!</h1>,document.getElementById('root'));
