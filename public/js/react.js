5280556c93d8ec9ef3b636a480d2894008ebbd0e
Josh <joshuscurtis@gmail.com>
2020-09-05 14:41:46 +0100

    react

--- public/js/react.js
+++ public/js/react.js
@@ -49,29 +49,10 @@ function Copyright() {
 
 function MyCard() {
 	return (
-	<div>
-	<Card variant="outlined">
-      <CardContent>
-        <Typography  color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-  </Card>
-  </div>
+		<div>
+			<Card variant="outlined">
+	 		</Card>
+  		</div>
 	);
 }
 
