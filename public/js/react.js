0074a716a23ac3a53111904f0d3d781f2161f0f3
Josh <joshuscurtis@gmail.com>
2020-09-05 13:41:24 +0100

    react.ejs

--- public/js/react.js
+++ public/js/react.js
@@ -1,2 +1,39 @@
+const {
+  colors,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+  Container,
+  Box,
+  SvgIcon,
+  Link,
+} = MaterialUI;
 
-ReactDOM.render(<h1>Hello, world!</h1>,document.getElementById('root'));
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+function App() {
+  return (
+    <Container maxWidth="sm">
+      <div style={{ marginTop: 24, }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          CDN v4-beta example
+        </Typography>
+        <Copyright />
+      </div>
+    </Container>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
