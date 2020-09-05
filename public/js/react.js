e829a86af868ce302988314c6338b953c7a66ad9
Josh <joshuscurtis@gmail.com>
2020-09-05 13:53:26 +0100

    react

--- public/js/react.js
+++ public/js/react.js
@@ -7,7 +7,44 @@ const {
   Box,
   SvgIcon,
   Link,
+  AppBar,
+  ToolBar,
+  MenuIcon,
+  IconButton,
 } = MaterialUI;
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
 
 
 function Copyright() {
@@ -27,6 +64,7 @@ function App() {
   return (
     <Container maxWidth="sm">
       <div style={{ marginTop: 24, }}>
+	  	<ButtonAppBar/>
         <Typography variant="h4" component="h1" gutterBottom>
           CDN v4-beta example
         </Typography>
