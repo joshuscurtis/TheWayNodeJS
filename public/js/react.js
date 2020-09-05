32ff9d8e538188410f0cd39e368836b61de29bce
Josh <joshuscurtis@gmail.com>
2020-09-05 14:34:50 +0100

    react

--- public/js/react.js
+++ public/js/react.js
@@ -12,7 +12,10 @@ const {
   MenuIcon,
   IconButton,
   Button,
-  Grid
+  Grid,
+  Card,
+  CardContents,
+  CardActions
 } = MaterialUI;
 
 
@@ -44,6 +47,35 @@ function Copyright() {
   );
 }
 
+function MyCard() {
+	return (
+	<Card variant="outlined">
+      <CardContent>
+        <Typography  color="textSecondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography color="textSecondary">
+          adjective
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+  </Card>
+	);
+}
+
+
+
+
 function App() {
   return (
 	<Container maxWidth="lg">
@@ -59,6 +91,8 @@ function App() {
 		        	<Typography variant="h4" component="h1" gutterBottom>
 		          		Left
 			  		</Typography>
+					</ MyCard>
+					</ MyCard>
 		        </Grid>
 		        <Grid item xs={6}>
 		        	<Typography variant="h4" component="h1" gutterBottom>
