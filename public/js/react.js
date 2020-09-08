6ced85829182c4320af54fde64e5650934635999
Josh <joshuscurtis@gmail.com>
2020-09-08 15:42:39 +0100

    react

--- public/js/react.js
+++ public/js/react.js
@@ -48,8 +48,8 @@ function CardApp(props) {
 	
   return (
 
-      <div class="OrderCard__container" style={{margin: 5,}}>
-		<Card class="OrderCard__main" style={{backgroundColor: props.isprocessing ? '#f0ad4e' : '#5cb85c',}} variant="outlined">
+      <div class="OrderCard__Main" style={{margin: 5,}}>
+		<Card style={{backgroundColor: props.isprocessing ? '#f0ad4e' : '#5cb85c',}} variant="outlined">
 			<CardHeader	title={cardTitle} subheader={props.time}>
 			</CardHeader>
 			<CardContent>
