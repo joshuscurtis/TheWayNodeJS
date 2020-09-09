7682467ec8ab0927ca728feccc812338e113b67a
Josh <joshuscurtis@gmail.com>
2020-09-09 11:26:51 +0100

    react

--- public/js/react.js
+++ public/js/react.js
@@ -94,14 +94,14 @@ function CardApp(props) {
 	const [close, setClose] = useState(false);
 	const [timer, setTimer] = useState(0);
 	
-	function timeCalc(createdTime)
+	function timeCalc(createdTime) {
 		var timeNow = Date.now();
 		var timeOpen = timeNow - createdTime;
 		timeOpen = new Date(timeOpen);
 		
 		var timeOpenStr = timeOpen.getMinutes() + "m " + timeOpen.getSeconds()+"s"
 	return (timeOpenStr);
-	
+	}
 	//calc time
 	useEffect(() => {
 		const interval = setInterval(() => {
