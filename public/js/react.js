875d4751794fda4c86242b11cf4cb2cbf3c06a2d
Josh <joshuscurtis@gmail.com>
2020-09-10 18:28:30 +0100

    logo

--- public/js/react.js
+++ public/js/react.js
@@ -261,7 +261,7 @@ function ButtonAppBar() {
     <div>
       <AppBar position="fixed">
         <Toolbar>
-			<img src="iorders.png" />
+			<img src="iorder.png" />
 			<Typography className="App_Title" align="center" variant="h4" component="h1" gutterBottom>
 		    	iOrders
 			</Typography>
